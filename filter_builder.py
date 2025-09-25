c68c799d1fbfcf6d45408256ed4c92271d3adefd
Timur Khvat <timurkhvat@gmail.com>
2025-09-25 15:57:09 +0200

    Add build_query utility to convert FilterResponseSchema into MongoDB queries

---
+++ filter_builder.py
@@ -0,0 +1,37 @@
+from schemas.filters import Operator, LogicalOperator, FilterResponseSchema
+
+
+def build_query(filter_data: FilterResponseSchema):
+    """
+    Utility function to convert FilterResponseSchema into a MongoDB query dictionary.
+    """
+
+    filter_conditions = []
+
+    for cond in filter_data.conditions:
+        field, op, value = cond.field, cond.operator, cond.value
+
+        if op == Operator.EQ:
+            filter_conditions.append({field: value})
+        elif op == Operator.NEQ:
+            filter_conditions.append({field: {"$ne": value}})
+        elif op == Operator.GT:
+            filter_conditions.append({field: {"$gt": value}})
+        elif op == Operator.GTE:
+            filter_conditions.append({field: {"$gte": value}})
+        elif op == Operator.LT:
+            filter_conditions.append({field: {"$lt": value}})
+        elif op == Operator.LTE:
+            filter_conditions.append({field: {"$lte": value}})
+        elif op == Operator.INCLUDE:
+            filter_conditions.append(
+                {field: {"$in": value if isinstance(value, list) else [value]}}
+            )
+        elif op == Operator.REGEX:
+            filter_conditions.append({field: {"$regex": value, "$options": "i"}})
+
+    return (
+        {"$and": filter_conditions}
+        if filter_data.logical_operator == LogicalOperator.AND
+        else {"$or": filter_conditions}
+    )
