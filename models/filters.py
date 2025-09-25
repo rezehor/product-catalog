fa0fd200d01f689e2f00477ba9b222f6e5a50e3a
Timur Khvat <timurkhvat@gmail.com>
2025-09-25 15:38:52 +0200

    Add Filter model with name, conditions and logical operator

---
+++ models/filters.py
@@ -0,0 +1,16 @@
+from typing import Any, Optional
+
+from beanie import Document, PydanticObjectId, Indexed
+from pydantic import Field
+
+from schemas.filters import LogicalOperator
+
+
+class Filter(Document):
+    id: PydanticObjectId = Field(default_factory=PydanticObjectId)
+    name: Indexed(str, unique=True)
+    conditions: list[dict[str, Any]]
+    logical_operator: LogicalOperator = LogicalOperator.AND
+
+    class Settings:
+        name = "filters"
